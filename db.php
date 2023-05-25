4009e2f7a360891524556e12185c62fa3829eccc
Antu Acharjee <35660520+iamantu93@users.noreply.github.com>
2023-05-25 13:26:09 +0600

    Create db.php

---
+++ db.php
@@ -0,0 +1,37 @@
+<?php
+
+// Create a database connection
+$connection=mysqli_connect("192.168.70.130","ecomuser","ecompassword","ecomdb","3306");
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $title = $_POST["name"];
+    $description = $_POST["description"];
+    $image = $_POST["image"];
+    
+    // Prepare an SQL statement
+    $stmt = $conn->prepare("INSERT INTO food_items (title, description, image) VALUES (?, ?, ?)");
+    
+    // Bind parameters to the statement
+    $stmt->bind_param("sss", $title, $description, $image);
+    
+    // Execute the statement
+    if ($stmt->execute()) {
+        echo "Form data submitted successfully.";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+    
+    // Close the statement
+    $stmt->close();
+}
+
+// Close the database connection
+$conn->close();
+?>
